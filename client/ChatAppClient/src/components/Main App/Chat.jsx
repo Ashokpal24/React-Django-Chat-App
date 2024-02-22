763c8ee6bcad31da6e75bbb21fd86d8b2d5d0e40
Ashok Pal <88579810+Ashokpal24@users.noreply.github.com>
2024-02-22 18:20:04 +0000

    Added basic UI.

--- client/ChatAppClient/src/components/Main App/Chat.jsx
+++ client/ChatAppClient/src/components/Main App/Chat.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import { Box, Button, TextField, List, ListItemButton, Avatar } from '@mui/material'
 
 const Chat = () => {
-    const [messages, setMessages] = useState([]);
+    const [messages, setMessages] = useState([
+        // { message: "Hello world", username: "User01" },
+        // { message: "What is uppp", username: "User02" },
+    ]);
     const [input, setInput] = useState('');
     const [roomName, setRoomName] = useState('');
     const [ws, setWs] = useState(null);
     const UserName = "User" + Math.floor(Math.random() * 100)
-
+    // const UserName = "User01"
     useEffect(() => {
         if (roomName) {
             const newWs = new WebSocket(`wss://silver-couscous-xqv4qqrpwxw345w-8000.app.github.dev/ws/${roomName}/`);
@@ -45,24 +49,97 @@ const Chat = () => {
     };
 
     return (
-        <div>
-            <div>
-                {messages.map((data, index) => (
-                    <div key={index}>
-                        Username: {data['username']}
-                        <br />
-                        Message: {data['message']}
-                    </div>
-                ))}
-            </div>
-            <input
-                type="text"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-            />
-            <button onClick={sendMessage}>Send</button>
-            <button onClick={createRoom}>Create Room</button>
-        </div>
+        <Box
+            sx={{
+                width: "98vw",
+                height: "95vh",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center"
+            }}
+        >
+            <Box sx={{
+                width: "600px",
+                height: "98vh",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <List sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    width: "100%",
+                    height: "50%",
+                    overflowY: "scroll",
+                    marginBottom: "1.5rem"
+                }}>
+                    {messages.map((data, index) => {
+                        const userTempName = data['username']
+                        return (
+                            <ListItemButton
+                                sx={{ alignSelf: userTempName == UserName ? "start" : "end" }}
+                                key={index}
+                            >
+                                <Box
+                                    sx={{
+                                        width: "200px",
+                                        display: "flex",
+                                        flexDirection: "row",
+                                        alignItems: "center",
+                                        justifyContent: "start"
+                                    }}>
+                                    <Avatar sx={{
+                                        backgroundColor: userTempName == UserName ? "green" : "rebeccapurple",
+                                        marginRight: "1.5rem",
+
+                                    }} >
+                                        {userTempName.charAt(0) + userTempName.charAt(userTempName.length - 1)}
+                                    </Avatar>
+                                    <Box>
+                                        {data['message']}
+                                    </Box>
+                                </Box>
+                            </ListItemButton>
+                        )
+                    })}
+                </List>
+                <TextField
+                    sx={{
+                        width: "100%",
+                        marginBottom: "1.5rem"
+                    }}
+                    label="Chat"
+                    variant="outlined"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
+                <Box
+                    sx={{
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        justifyContent: "end"
+                    }}>
+                    <Button
+                        sx={{ marginRight: "1.5rem" }}
+                        variant="outlined"
+                        color="secondary"
+                        onClick={sendMessage}
+                    >
+                        Send
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        onClick={createRoom}
+                    >
+                        Create Room
+                    </Button>
+                </Box>
+            </Box>
+        </Box >
     );
 };
 
