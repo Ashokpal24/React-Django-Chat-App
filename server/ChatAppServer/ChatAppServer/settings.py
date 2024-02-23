79b93682486d343864594e76fc8a9e06fb35e078
Ashok Pal <88579810+Ashokpal24@users.noreply.github.com>
2024-02-23 11:45:03 +0000

    Added user model and updated settings for JWT token.

--- server/ChatAppServer/ChatAppServer/settings.py
+++ server/ChatAppServer/ChatAppServer/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -14,11 +16,15 @@ SECRET_KEY = "django-insecure-hg-+x(84@n5%0h5s20o-c&sewp^ijlsb3oyonzts@r@=jgly51
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "https://silver-couscous-xqv4qqrpwxw345w-5173.app.github.dev"
+    "https://silver-couscous-xqv4qqrpwxw345w-5173.app.github.dev",
+    "5j85dm-5173.csb.app",
+    "5j85dm-8000.csb.app"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://silver-couscous-xqv4qqrpwxw345w-5173.app.github.dev"
+    "https://silver-couscous-xqv4qqrpwxw345w-5173.app.github.dev",
+    "https://5j85dm-5173.csb.app",
+    "https://5j85dm-8000.csb.app"
 ]
 
 
@@ -34,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +133,37 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# JWT Configuration
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+}
+
+# Email Configuration
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_USE_TLS = True
--- server/ChatAppServer/ChatCore/models.py
+++ server/ChatAppServer/ChatCore/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, name, password=None, password2=None):
+        if not name:
+            raise ValueError("User must have username")
+        user = self.model(name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, name, password=None):
+        user = self.create_user(
+            name=name,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=255)
+
+    USERNAME_FIELD = 'name'
+    REQUIRED_FIELDS = ['name']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f'Username: {self.name}'
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
